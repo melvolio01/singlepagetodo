f8c398318bc0aa58a9df32ad202281af15630910
Andy Hunt <andyhunt353@gmail.com>
2017-10-01 19:38:52 +0000

    Post route added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express'),
         //Setting the 'app' as express functionality
         app = express(),
-        port = process.env.PORT || 3000;
+        port = process.env.PORT || 3000,
+        bodyParser = require("body-parser");
+        
 
 var todoRoutes = require('./routes/todo'); 
- 
+
+//allow us to access the req.body for requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function(req, res){
     //alternatively could use res.send(), which calls res.json()
    res.json({message: "Hi there from Express"}); 
