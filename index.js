affc2d25ea4842ae2ca7fc0d6892d1d191ee2181
Andy Hunt <andyhunt353@gmail.com>
2017-10-02 11:48:42 +0000

    Api up and running, basic front-end

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ var express = require('express'),
         bodyParser = require("body-parser");
         
 
+//requires routes
 var todoRoutes = require('./routes/todo'); 
 
 //allow us to access the req.body for requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
 
+//So, basically this is the route where the front end happens :-)
 app.get('/', function(req, res){
     //alternatively could use res.send(), which calls res.json()
-   res.json({message: "Hi there from Express"}); 
+  res.sendFile("index.html"); 
 });
 
-//adds the prefix to all of the todoRoutes we make
+//Whereas this is the backend and adds all our api routes, hence api :-)
 app.use('/api/todo', todoRoutes);
        
 //Sets app to 'listen' on a particular port - default being 8080
--- routes/todo.js
+++ routes/todo.js
@@ -2,29 +2,17 @@ var express = require('express');
 var router = express.Router();
 //go to mongoose and find all todos
 var db = require('../models');
+var helpers = require('../helpers/todos');
 
+//this combines the original index and create routes for todos into one route 
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
 
-router.get('/', function(req, res){
-    //find all todos
-    db.Todo.find()
-    //promise, insted of (req, res) etc to 'respond' with all todos in json format
-    .then(function(todos){
-       res.json(todos); 
-    })
-    //'catching' any errors and responding with them
-    .catch(function(err){
-       res.send(err); 
-    });
-});
-
-router.post('/', function(req, res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
+//combining the :todoId routes into 1
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
