7d53a1e651c208f039bc1b0bab4911b7b02a28d3
Andy Hunt <andyhunt353@gmail.com>
2017-10-02 16:07:46 +0000

    Added mlab

--- models/index.js
+++ models/index.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 //this allows us to see what is happening with program at any given point
 mongoose.set('debug', true);
 //name for this url doesn't really matter - if it doesn't already exist it will be created for us
-mongoose.connect('mongodb://localhost/todo-api');
+// mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://melvin:onepage@ds159274.mlab.com:59274/singlepagetodo');
+
 
 //allows us to use the mongoose promise library
 mongoose.Promise = Promise;
